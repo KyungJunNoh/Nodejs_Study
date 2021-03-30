c02fe1075c7f5505c6b9dfc0e0f4f684b43c5f97
KyungJunNoh <developerjun0615@gmail.com>
2021-03-30 17:14:14 +0900

    [Session 수정]

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const session = require('express-session');
 // const { Select } = require('../db/mysql-db');
 var router = express.Router();
 var mysqldb = require('../db/mysql-db');
@@ -13,13 +14,11 @@ router.post('/login', function (req, res, next) {
     mysqldb.SQLconnect();
     mysqldb.Select(userId,userPw,function(rows, err){
         if(rows != "") {
-            res.cookie("ID",req.body.userId);
-            console.log(req.session);
-            res.json({
-                "code": "200",
-                "data" : userId
-            },);
-            console.log("로그인 성공");
+            req.session.userid = userId;
+            req.session.save(function(){
+                res.json({"data" : "success"});
+            });
+            console.log(req.session.userid);
         } else {
             res.json({"data" : "실패"});
             console.log("로그인 실패");
@@ -29,10 +28,17 @@ router.post('/login', function (req, res, next) {
 });
 
 router.post('/logout',function(req,res,err){
-    console.log(req.session);
-    req.session.destroy();
-    res.clearCookie('sid');
-    console.log(req.session);
-    res.send("ok");
+    // req.session.destroy();
+    // res.clearCookie(); // 세션 쿠키 삭제
+    console.log('로그아웃 처리');
+    req.session.destroy(
+    function (err) {
+        if (err) {
+            console.log('세션 삭제시 에러');
+            return;
+        }
+        res.json({"data" : "success"});
+    });//세션정보 삭제    
 })
+
 module.exports = router;
