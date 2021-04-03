d7e57fa11ee60a108e23e1875b8b13d557c6e376
KyungJunNoh <developerjun0615@gmail.com>
2021-04-03 16:48:16 +0900

    [게시판 구현 완료]

--- db/mysql-db.js
+++ db/mysql-db.js
@@ -27,44 +27,74 @@ module.exports = {
   },
   Select : function(userId,userPw,callback){
     connection.query('select * from test_user where id=? and pw = ?',[userId,userPw],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[SELECT 완료]");
       callback(rows, err);
     });
   },
   Insert : function(userId,userPw,callback){
     connection.query('insert into test_user(id,pw)values(?,?)',[userId,userPw],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[Insert 완료]");
       callback(rows, err);
     });
   },
   Update : function(userId,userPwNew,callback){
     connection.query('UPDATE test_user SET pw=? WHERE id=?',[userPwNew,userId],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[Update 완료]");
       callback(rows, err);
     })
   },
   Delete : function(userId,callback){
     connection.query("DELETE FROM test_user WHERE id=?",[userId],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[Delete 완료]");
       callback(rows, err);
     })
   },
+  boardAllSelect :function(callback){
+    connection.query("Select * from board",function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
+      console.log("[boardAllSelect 완료]");
+      callback(rows,err);
+    })
+  },
+  boardSelect :function(idx,callback){
+    connection.query("Select * from board WHERE idx = ?",[idx],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
+      console.log("[boardSelect 완료]");
+      callback(rows,err);
+    })
+  },
   boardInsert : function(title,content,callback){
     connection.query("INSERT INTO board(title,content)values(?,?)",[title,content],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[boardInsert 완료]");
       callback(rows,err);
     })
   },
   boardUpdate : function(title,content,idx,callback){
     connection.query("UPDATE board SET title=?, content=? WHERE idx=?",[title,content,idx],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[boardUpdate 완료]");
       callback(rows, err);
     })
   },
   boardDelete : function(idx,callback){
     connection.query("DELETE FROM board WHERE idx = ?",[idx],function(err,rows){
+      connection.query("SET @CNT = 0");
+      connection.query("UPDATE board SET board.idx = @CNT:=@CNT+1");
       console.log("[boardDelete 완료]");
       callback(rows,err);
     })
-  } 
+  }
 }
--- routes/board.js
+++ routes/board.js
@@ -2,12 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 var mysqlDB = require('../db/mysql-db');
+var path = require('path');
 
-router.get('/', function (req, res, next) {
-    res.send("hello board");
+router.get('/', function (req, res, next) { // 게시판 글 전체 조회
+    mysqlDB.boardAllSelect(function(rows,err){
+        console.log(rows);
+        if(!err){
+            res.json({"data" : rows}); // 게시판 글 목록 전체 조회
+        }else{
+            res.json(err);
+        }
+    })
 });
 
-router.post('/insert', function (req, res, next) {
+router.get('/:pageid', function (req, res, next) { // 게시판 글 단일 조회
+    var idx = path.parse(req.params.pageid).base;
+    mysqlDB.boardSelect(idx,function(rows,err){
+        console.log(rows);
+        if(!err){
+            res.json({"data" : rows}); // 게시판 글 목록 단일 조회
+        }else{
+            res.json(err);
+        }
+    })
+});
+
+router.post('/insert', function (req, res, next) { // 게시판 글 생성
     var title = req.body.title;
     var content = req.body.content;
     mysqlDB.boardInsert(title,content,function(rows,err){
@@ -22,7 +42,7 @@ router.post('/insert', function (req, res, next) {
     })
 });
 
-router.post('/update', function (req, res, next) {
+router.put('/update', function (req, res, next) { // 게시판 글 수정
     var idx = req.body.idx;
     var title = req.body.title;
     var content = req.body.content;
@@ -38,7 +58,7 @@ router.post('/update', function (req, res, next) {
     })
 });
 
-router.post('/delete',function(req,res,err){
+router.delete('/delete',function(req,res,err){ // 게시판 글 삭제
     var idx = req.body.idx;
     mysqlDB.boardDelete(idx,function(rows,err){
         console.log(rows);
